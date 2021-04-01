fb5411953432975d37f3beeaea297fdd8af16306
yeelone <yljckh@gmail.com>
2021-04-02 00:00:31 +0800

    fixed

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -6,16 +6,16 @@
 // const config = {
 //     version: '1.1.1',
 //     title: '工资模板计算系统',
-//     baseurl : 'http://127.0.0.1:8080',
-//     api: 'http://127.0.0.1:8080/api'
+//     baseurl : 'http://127.0.0.1:8090',
+//     api: 'http://127.0.0.1:8090/api'
 // }
 
 // for production 腾讯云
 const config = {
     version: '1.1.1',
     title: '工资模板计算系统',
-    baseurl : 'http://47.104.154.41:8080',
-    api: 'http://47.104.154.41:8080/api'
+    baseurl : 'http://47.104.154.41:8090',
+    api: 'http://47.104.154.41:8090/api'
 }
 
 
